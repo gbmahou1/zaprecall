2f0d8f6bff39fe2419987a408e207b549ec155a4
gbmahou1 <gbmahou1@hotmail.com>
2021-12-09 00:22:18 -0300

    Layout da Primeira Tela

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/* import ReactDOM from "react-dom";
+
+function Lista() {
+	return (
+		<div>
+			<h1>Lista de Compras</h1>
+			<ul>
+				<li>Pão</li>
+				<li>Banana</li>
+				<li>Milk Shake de Doce de Leite</li>
+				<li>Nutella</li>
+				<li>Sorvete</li>
+			</ul>
+		</div>
+	);
+}
+
+const lista = Lista();
+
+ReactDOM.render(lista, document.querySelector(".root")); */
